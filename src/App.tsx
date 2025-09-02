97c040f3d4591632dfd39fb280ce0a7e0e6cd55b
Lakshay Bhatia <b.lakshay.aspect@gmail.com>
2025-09-02 18:37:11 +0530

    changed fonts + exception page completed + side bar functionality done

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,20 @@
 
 import './App.css'
-import { Link, Route, Routes, Navigate } from 'react-router-dom'
+import { Link, Route, Routes,  useNavigate } from 'react-router-dom'
 import { BellIcon, UserRound, ArrowLeftToLine } from 'lucide-react'
 import Dashboard from './pages/Dashboard.tsx'
 import Cctv from './pages/Cctv.tsx'
 import PageNotFound from './pages/PageNotFound.tsx'
 import { useState } from 'react'
 function App() {
-  const [collapse, setCollapse] = useState(true)
+  const [collapse, setCollapse] = useState(true);
+  const navigate = useNavigate();
   return (
-    <div>
+    <div className='font-[work-sans]'>
       <div className='flex h-screen bg-background'>
         <div className={`flex  bg-slate-950 text-white border-r-3 border-white/50 ${collapse ? 'w-1/20' : 'w-1/6'} transition-all duration-300`}>
-          <div>
-            <div onClick={() => setCollapse(!collapse)} className='flex px-2 py-2 h-10 min-w-10 hover:bg-slate-800 rounded-md ml-4 mt-6 items-center justify-around cursor-pointer gap-5 items-center'>
+          <div className='w-full'>
+            <div onClick={() => setCollapse(!collapse)} className='flex px-2 py-2 hover:bg-slate-800 duration-300 bg-slate-900 rounded-md mx-4 mt-4 cursor-pointer gap-5 items-center'>
               <ArrowLeftToLine className={`${collapse ? 'rotate-180' : ''} transition-all duration-300`} /> 
               <span className={`${collapse ? 'hidden' : ''} text-2xl font-bold`}>Menu</span>
             </div>
@@ -22,8 +23,8 @@ function App() {
           
         </div>
         <div className='flex-1 overflow-auto'>
-          <nav className='flex items-center justify-between p-6 border-b border-border bg-blue-950/100 text-white border-b-3 border-white/50'>
-            <div className='text-2xl font-semibold'>TrafficAI</div>
+          <nav className='flex items-center justify-between px-6 py-4 border-b border-border bg-[#1E293B] text-white border-b-3 border-white/50'>
+            <div className='text-3xl font-semibold cursor-pointer' onClick={() => navigate('/')} >Traffi<span className='text-4xl font-bold text-[#EF4444]'>X</span></div>
               <div className='flex gap-8'>
                 <BellIcon/>
                 <UserRound/>
--- src/pages/PageNotFound.tsx
+++ src/pages/PageNotFound.tsx
@@ -3,10 +3,10 @@ import { useNavigate } from 'react-router-dom'
 export default function PageNotFound() {
     const navigate = useNavigate()
     return (
-      <div style={{ padding: '12px' }}>
-        <h1>Page Not Found</h1>
-        <p>Page Not Found</p>
-        <button onClick={() => navigate('/')}>Return to Home</button>
+      <div className='flex flex-col justify-center items-center h-full bg-[#F9FAFB]'>
+        <span className='text-5xl font-bold text-[#111827]  '>Page Not Found</span>
+        <p className='mt-3 text-xl text-[#6B7280]'>Looks like we couldn't find that.</p>
+        <button className='bg-[#3B82F6] p-3 text-white rounded-xl mt-10 cursor-pointer hover:bg-[#2563EB] duration-300' onClick={() => navigate('/')}>Return to Home</button>
       </div>
     )
   }
